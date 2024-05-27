93289679aa5caa0f78597ee4eba4a4c84b910dae
Spencer Elkington <spelkington@gmail.com>
2024-05-26 19:24:43 -0700

    fix: zprofile edit for foreman.zsh

--- utilities/setup/macos/helpers/foreman.zsh
+++ utilities/setup/macos/helpers/foreman.zsh
@@ -1,9 +1,10 @@
 # Installs Foreman for roblox project dependencies.
 #
-# Install from Foreman repo: https://github.com/Roblox/foreman
+# Installed from Foreman repo: https://github.com/Roblox/foreman
 
 echo "[INFO] Installing Foreman for Roblox project dependencies..."
 if [-x $(cargo install foreman)]; then
+  echo "export PATH=\$HOME/.foreman/bin:\$PATH" >> ~/.zprofile
   echo "[SUCCESS] Foreman installed!"
   exit 0
 else
